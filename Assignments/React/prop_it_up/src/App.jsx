78435cb0c3271e686c3949c856a8234135bb2451
Jared_P <jaredpisell@gmail.com>
2023-01-13 07:55:46 -0800

    Part 2 of prop it up complete

--- Assignments/React/prop_it_up/src/App.jsx
+++ Assignments/React/prop_it_up/src/App.jsx
@@ -1,33 +1,48 @@
+import { useState } from 'react';
 import Card from './Card.jsx';
 
-const data = [
-  {
-    name: 'Doe Jane',
-    age: 45,
-    hair: 'Black',
-  },
-  {
-    name: 'Smith, John',
-    age: 88,
-    hair: 'Brown',
-  },
-  {
-    name: 'Fillmore, Millard',
-    age: 88,
-    hair: 'Brown',
-  },
-  {
-    name: 'Smith, Maria',
-    age: 88,
-    hair: 'Brown',
-  },
-];
-
 function App() {
+  const [data, setData] = useState([
+    {
+      name: 'Doe Jane',
+      age: 45,
+      hair: 'Black',
+    },
+    {
+      name: 'Smith, John',
+      age: 88,
+      hair: 'Brown',
+    },
+    {
+      name: 'Fillmore, Millard',
+      age: 88,
+      hair: 'Brown',
+    },
+    {
+      name: 'Smith, Maria',
+      age: 88,
+      hair: 'Brown',
+    },
+  ]);
+
+  const changeAge = (targetIdx) => {
+    const newData = data.map((person, idx) => {
+      //Only fires if idx matches person needed during mapping to find/change person's age.
+      if (idx === targetIdx) {
+        person = {
+          ...person,
+          age: person.age + 1,
+        };
+      }
+      return person;
+    });
+    setData(newData); //<-- State altered post data update.
+  };
+
   return (
     <div className="App">
-      {data.map((item) => {
-        return <Card name={item.name} age={item.age} hair={item.hair} />;
+      {data.map((item, idx) => {
+        return <Card name={item.name} age={item.age} hair={item.hair} changeAge={changeAge} idx={idx} key={idx} />;
       })}
     </div>
   );
--- Assignments/React/prop_it_up/src/Card.jsx
+++ Assignments/React/prop_it_up/src/Card.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-
 const Card = (props) => {
-  const { name, age, hair } = props;
+  const { name, age, hair, changeAge, idx } = props;
 
   return (
     <div>
       <h1>Name: {name}</h1>
+      <p>{idx}</p>
       <p>Age: {age}</p>
       <p>Hair Color: {hair}</p>
+      <button onClick={() => changeAge(idx)}>Birthday!</button>
     </div>
   );
 };
