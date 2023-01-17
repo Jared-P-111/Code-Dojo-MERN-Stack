60868967e9bbe7d0db61439bbcdbaab1e3f1eeff
Jared_P <jaredpisell@gmail.com>
2023-01-16 18:27:43 -0800

    Finished More Forms

--- Assignments/React/hook_form/src/Components/Form.jsx
+++ Assignments/React/hook_form/src/Components/Form.jsx
@@ -3,23 +3,22 @@ import styled from 'styled-components';
 import FormInput from './FormInput';
 import { useState } from 'react';
 
-const StyledForm = styled.div`
-  height: 50vh;
-`;
+const StyledForm = styled.div``;
 
 const Form = () => {
   const [data, setData] = useState([
-    { 'First Name': '' },
-    { 'Last Name': '' },
-    { Email: '' },
-    { Password: '' },
-    { 'Confirm Password': '' },
+    { 'First Name': ['', false, 'Field must be at least 2 characters long'] },
+    { 'Last Name': ['', false, 'Field must be at least 2 characters long'] },
+    { Email: ['', false, 'Field must be at least 5 characters long'] },
+    { Password: ['', false, 'Your passwords must match'] },
+    { 'Confirm Password': ['', false, 'Your passwords must match'] },
   ]);
 
   return (
     <StyledForm>
-      <form>
+      <form type="submit">
         <FormInput data={data} setData={setData} />
+        <button className="btn btn-primary">Submit Form</button>
       </form>
     </StyledForm>
   );
--- Assignments/React/hook_form/src/Components/FormInput.jsx
+++ Assignments/React/hook_form/src/Components/FormInput.jsx
@@ -2,22 +2,71 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledInput = styled.div`
-  display: flex;
-  color: #212529;
+  color: #111112;
   font-weight: bold;
-  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
   margin-bottom: 30px;
+  justify-content: space-between;
 
   & input {
     margin-left: 30px;
   }
+
+  & .inputContainer {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  & p {
+    font-weight: normal;
+  }
 `;
 
 const FormInput = ({ data, setData }) => {
+  //🧈 ------------- AUTH HANDLER FUNCTION -------------
   const updateData = (targetIdx, value, currItemName) => {
     const newData = data.map((item, idx) => {
       if (idx === targetIdx) {
-        item[currItemName] = value;
+        switch (currItemName[0]) {
+          case 'First Name':
+            if (value.length > 2) {
+              console.log(`First Name criteria met`);
+              item[currItemName][1] = true;
+              item[currItemName][0] = value;
+            }
+            break;
+          case 'Last Name':
+            if (value.length > 2) {
+              console.log('Last Name criteria met');
+              item[currItemName][1] = true;
+              item[currItemName][0] = value;
+            }
+            break;
+          case 'Email':
+            if (value.length > 5) {
+              console.log('Email criteria met');
+              item[currItemName][1] = true;
+              item[currItemName][0] = value;
+            }
+            break;
+          case 'Password':
+            if (value.length > 8) {
+              console.log('Password criteria met');
+              item[currItemName][1] = true;
+              item[currItemName][0] = value;
+            }
+            break;
+          case 'Confirm Password':
+            if (value.length > 8) {
+              console.log('Confirm Password criteria met');
+              item[currItemName][1] = true;
+              item[currItemName][0] = value;
+            }
+            break;
+          default:
+            break;
+        }
       }
       return item;
     });
@@ -25,33 +74,35 @@ const FormInput = ({ data, setData }) => {
   };
 
   return (
-    <div style={{ color: '#212529', fontWeight: 'bold' }}>
+    <div>
       {data.map((item, idx) => {
         let currItemName = Object.keys(item);
+        let currItemValue = Object.values(item);
+
         return (
           <div key={idx}>
             <StyledInput>
-              <label htmlFor={currItemName}>{currItemName}</label>
-              <input
-                type="text"
-                name={currItemName}
-                id={currItemName}
-                onChange={(e) => updateData(idx, e.target.value, currItemName)}
-              />
+              <div className="inputContainer">
+                <label htmlFor={currItemName}>{currItemName}</label>
+                <input
+                  type="text"
+                  name={currItemName}
+                  id={currItemName}
+                  onChange={(e) => updateData(idx, e.target.value, currItemName)}
+                />
+              </div>
+              <p>{!currItemValue[0][1] ? currItemValue[0][2] : ''}</p>
             </StyledInput>
           </div>
         );
       })}
-      <h5 style={{ textDecoration: 'underline' }}>Your Details</h5>
-      {data.map((item) => {
+      <h5 style={{ textDecoration: 'underline', marginTop: '40px' }}>Your Details</h5>
+      {data.map((item, idx) => {
+        let currItemValue = Object.values(item);
         return (
-          <>
-            <div>{item['First Name']}</div>
-            <div>{item['Last Name']}</div>
-            <div>{item['Email']}</div>
-            <div>{item['Password']}</div>
-            <div>{item['Confirm Password']}</div>
-          </>
+          <div key={idx}>
+            <p>{currItemValue[0][0]}</p>
+          </div>
         );
       })}
     </div>
