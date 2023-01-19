3e7dac1e1877b6ebe582e6e9ac11c420b2cb8fbe
Jared_P <jaredpisell@gmail.com>
2023-01-19 14:31:40 -0800

    Updated project for final touch

--- Assignments/React/luke_api_walker/src/components/CharacterSingle.jsx
+++ Assignments/React/luke_api_walker/src/components/CharacterSingle.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import obiWan from '../images/obiwan-04.png';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -6,6 +7,7 @@ import axios from 'axios';
 function CharacterSingle() {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
@@ -14,14 +16,22 @@ function CharacterSingle() {
         console.log(res.data);
         setCharacter(res.data);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        setError(true);
+      });
   }, [id]);
 
   return (
     <div className="card">
-      {character && (
+      {!error && character ? (
         <div className="card-body">
           <h1>Name: {character.name}</h1>
+        </div>
+      ) : (
+        <div className="card-body">
+          <h1>Obi-Wan says...</h1>
+          <img src={obiWan} alt="person" className="img-fluid" />
+          <h3 className="mt-4">These are not the drones you are looking for...</h3>
         </div>
       )}
     </div>
